8910ab5bb814d226b88d3104d0e32d51392639f6
Craig Thomas <craig@tinybigideas.com>
2013-07-20 18:17:38 -0400

    Initial angular set-up

---
+++ js/angular/app.js
@@ -0,0 +1,29 @@
+'use strict';
+
+
+/*------------------------------------*\
+   CONTENTS
+\*------------------------------------*/
+/*
+
+APP
+ - 
+ 
+*/
+
+
+/*------------------------------------*\
+   APP
+\*------------------------------------*/
+
+// Create a new app
+var app = angular.module('dungeon', []);
+ 
+// register a new service
+app.value('appName', 'Dungeon Crawler');
+ 
+// configure existing services inside initialization blocks.
+app.config(function($locationProvider) {
+      // Configure existing providers
+  	$locationProvider.hashPrefix('!');
+});
---
+++ js/angular/services.js
@@ -0,0 +1,26 @@
+'use strict';
+
+
+/*------------------------------------*\
+   CONTENTS
+\*------------------------------------*/
+/*
+
+SERVICES
+ - 
+ 
+*/
+
+
+/*------------------------------------*\
+   SERVICES
+    Functions for both controllers and directives
+\*------------------------------------*/
+
+//  service
+app.factory('$', function() {
+
+    return {
+        
+	}
+});
