0109734b9fb08227306bd86cb740dcbd2ec5e7f2
Jonjoe Whitfield <thedigitaldinlo@gmail.com>
2013-07-21 05:14:24 +0100

    add keybind script to index

--- js/angular/services.js
+++ js/angular/services.js
@@ -19,26 +19,7 @@ SERVICES
 
 //  service
 app.factory('$keys', function() {
-
-	$(document).keydown(function(e){
-	if (e.keyCode == 87){ // up = 87
-       startPos-= 16;
-       console.log(cords);
-    }
-    if (e.keyCode == 83){ // down = 83
-      startPos += 16;
-      console.log(cords); 
-    }
-    if(e.keyCode == 68) { // right = 68
-       startPos += 1;
-       console.log(cords);
-    }
-    if (e.keyCode == 65){ // left = 65
-       startPos -= 1;
-       console.log(cords);
-	});
-
     return {
-        
+		
 	}
 });
